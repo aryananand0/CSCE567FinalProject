5f31c1436b0b5935da251922ebbdf17f2cd1e4b7
aryananand0 <147997531+aryananand0@users.noreply.github.com>
2024-04-26 02:10:45 -0400

    Update 8_📈_Nationality Vs Market Value.py

--- pages/8_📈_Nationality Vs Market Value.py
+++ pages/8_📈_Nationality Vs Market Value.py
@@ -18,7 +18,9 @@ def load_data():
     return df
 
 df = load_data()
-
+st.title("Nationality by Market Value")
+st.write("This graph presents a collection of small bar graphs, each representing the market value of players from the selected nationality. By visualizing these individual player values, this graph aids in understanding the distribution of value within a nation. It allows us to discern whether a nation's high valuation is due to a few exceptionally valued players or if the overall player cohort contributes to this high value. Furthermore, this graph provides insights into which nations are most valued in terms of player market worth. By examining the distribution of player values across different nationalities, we can identify trends and patterns that highlight the strength of football talent in various countries. This analysis helps paint a clearer picture of the global football landscape, showcasing the diversity and depth of talent from different parts of the world.")
+st.write("To interact meaningfully with this graph, utilize the sidebar to select a specific nationality and delve into the distribution of player values within that country. This interactive feature allows you to dynamically explore how player values are distributed across different nationalities. By selecting a country of interest, you can gain insights into the composition of player values, whether it's dominated by a few high-value players or a more evenly distributed range of values across the player cohort")
 # Create sidebar for choosing nation
 chosen_country = st.sidebar.selectbox('Choose Country:', df['nationality'].unique())
 
