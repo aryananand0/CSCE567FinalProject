06476bade6cfd2e045283b565e613c57782beaf5
aryananand0 <147997531+aryananand0@users.noreply.github.com>
2024-04-26 02:11:08 -0400

    Update 9_📈_Club Market Value Comparison.py

--- pages/9_📈_Club Market Value Comparison.py
+++ pages/9_📈_Club Market Value Comparison.py
@@ -19,7 +19,6 @@ def plot_bar_chart(data, selected_leagues):
         y=alt.Y('price', title='Market Value'),
         tooltip=['club', 'price']
     ).properties(
-        title='Club Market Value Comparison',
         width=700,
         height=400
     ).interactive()
@@ -31,11 +30,15 @@ data = load_data()
 
 # Display the data table
 st.title('Club Market Value Comparison')
+st.write("This visualization serves as a comprehensive tool for understanding the value distribution among clubs within the selected league. Its primary function is to depict the relative values of all clubs, enabling us to identify the most valuable club in each league. Additionally, this visualization facilitates the assessment of value disparities within the league, shedding light on the economic dynamics and financial strengths of individual clubs.")
+st.write("Furthermore, this visualization offers a valuable opportunity to compare leagues directly. By juxtaposing multiple leagues side by side and examining the values of their respective clubs, users can gain insights into the comparative financial landscapes of different football leagues. This comparative analysis extends beyond individual clubs to provide a broader perspective on the economic competitiveness and market dynamics across various football leagues.")
+st.write("In essence, this visualization empowers users to explore and analyze the financial aspects of football leagues, offering a nuanced understanding of club values, league disparities, and inter-league comparisons.")
+st.write("To intact with this visualization you only have to choose what league or leagues you want to visualize and it will make a bar graph visualizing it for you")
+st.write("")
 
 
 # Sidebar for selecting leagues
 selected_leagues = st.sidebar.multiselect('Select Leagues', data['league'].unique())
 
 # Plot the bar chart
-st.write("## Vertical Bar Chart")
 st.altair_chart(plot_bar_chart(data, selected_leagues))
